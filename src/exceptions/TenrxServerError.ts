1488aae37219769bfb25b295247dd272d05fa9a1
Keyboard <keyboard@imperium.news>
2021-11-28 01:36:44 -0500

    Implemented exceptions.

---
+++ src/exceptions/TenrxServerError.ts
@@ -0,0 +1,29 @@
+/**
+ * Represents a Tenrx server error.
+ *
+ * @export
+ * @class TenrxServerError
+ * @extends {Error}
+ */
+export default class TenrxServerError extends Error {
+    /**
+     * The status code of the error.
+     *
+     * @type {number}
+     * @memberof TenrxServerError
+     */
+    statusCode: number;
+
+    /**
+     * Creates an instance of TenrxServerError.
+     * @param {string} message - The error message.
+     * @param {number} statusCode - The status code of the error.
+     * @memberof TenrxServerError
+     */
+    constructor(message: string, statusCode: number) {
+        super(message);
+        Object.setPrototypeOf(this, TenrxServerError.prototype);
+        this.name = "TenrxServerError";
+        this.statusCode = statusCode;
+    }
+}
