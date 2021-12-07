0bef72b6ee9ba3128994f4ae8160220a79c5458b
Keyboard <keyboard@imperium.news>
2021-12-07 02:20:23 -0500

    Fixed identation in registration test suite.

--- automatedtests/TenrxRegistration.test.ts
+++ automatedtests/TenrxRegistration.test.ts
@@ -6,33 +6,33 @@ Testlogger.setSettings({
 })
 
 test('CheckIfEmailExists Test Success', async () => {
-    const result = await checkIfEmailExists(TEST_USERNAME_EXISTS);
-    expect(result).toBe(true);
+  const result = await checkIfEmailExists(TEST_USERNAME_EXISTS);
+  expect(result).toBe(true);
 });
 
 test('CheckIfEmailExists Test Fail', async () => {
-    const result = await checkIfEmailExists(TEST_USERNAME_NOT_EXISTS);
-    expect(result).toBe(false);
+  const result = await checkIfEmailExists(TEST_USERNAME_NOT_EXISTS);
+  expect(result).toBe(false);
 });
 
 test('SaveSecurityAnswers Test Success', async () => {
-    const result = await saveSecurityQuestionAnswers(TEST_USERNAME_EXISTS, TEST_PASSWORD_SUCCESS, [
-      {
-        id: 0,
-        questionID: 19,
-        answer: 'Smith',
-      },
-      {
-        id: 0,
-        questionID: 20,
-        answer: 'Darky',
-      },
-      {
-        id: 0,
-        questionID: 21,
-        answer: 'Mark',
-      }
-    ]);
-    expect(result).not.toBeNull();
-    expect(result.status).toBe(200);
+  const result = await saveSecurityQuestionAnswers(TEST_USERNAME_EXISTS, TEST_PASSWORD_SUCCESS, [
+    {
+      id: 0,
+      questionID: 19,
+      answer: 'Smith',
+    },
+    {
+      id: 0,
+      questionID: 20,
+      answer: 'Darky',
+    },
+    {
+      id: 0,
+      questionID: 21,
+      answer: 'Mark',
+    }
+  ]);
+  expect(result).not.toBeNull();
+  expect(result.status).toBe(200);
 });
