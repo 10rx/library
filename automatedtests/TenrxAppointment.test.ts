3664eeeb4c21e24d56f8e6cd2e509024dd246768
Keyboard <keyboard@imperium.news>
2022-03-23 00:48:38 -0400

    Added empty test case for appointment to be implemented at a later date.

---
+++ automatedtests/TenrxAppointment.test.ts
@@ -0,0 +1,11 @@
+import { Testlogger } from './includes/TexrxCommonInclude.js';
+
+Testlogger.setSettings({
+  type: 'pretty',
+});
+
+// TODO IMPLEMENT TEST FOR APPOINTMENTS.
+
+test('Appointment Test Successful', () => {
+  expect("").not.toBeNull();
+});
